04a43fa3b1fa818c9fe707106da35a5968e172ef
zouzirun <zouzirun@163.com>
2025-04-15 13:56:42 +0800

    添加sqlite test

---
+++ download/sqlite3_build.sh
@@ -0,0 +1,8 @@
+tar zxvf sqlite-autoconf-3490100.tar.gz -C ./temp/
+cd ./temp/sqlite-autoconf-3490100
+# ./configure --prefix=/usr/local/sqlite3  --host=arm-linux-gnu
+./configure --prefix=/home/zirun/work/RUN_GATEWAY/libs/sqlite3
+make
+make install
+cd ../..
+rm -rf ./temp/sqlite-autoconf-3490100
--- src/main.c
+++ src/main.c
@@ -2,25 +2,74 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/select.h>
-#include "linenoise.h"
-#include <sqlite3.h>
-
+// #include "linenoise.h"
+#include "sqlite3.h"
+#define DATABASE_NAME                 "MyDBDemo.db"
+int callback(void* ,int nCount,char** pValue,char** pName)
+{
+    char print_str[128];
+    for(int i=0;i<nCount;i++)
+    {
+      sprintf(print_str,"[%s] = %s\n",pName[i],pValue[i]);       
+    }
+    printf("%s", print_str);
+    return 0;  
+}
 int main() {
    sqlite3* db;
-   int rc = sqlite3_open(":memory:", &db);
+   int rc = sqlite3_open(DATABASE_NAME, &db);
 
    if (rc != SQLITE_OK) {
-      // 处理数据库打开失败的情况
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return 1;
    }
 
-   // 执行数据库操作
+   // Create table
+   char* err_msg = 0;
+   const char* create_table_sql = 
+      "CREATE TABLE IF NOT EXISTS Users("
+      "Id INTEGER PRIMARY KEY AUTOINCREMENT,"
+      "Name TEXT NOT NULL,"
+      "Age INTEGER);";
+   
+   rc = sqlite3_exec(db, create_table_sql, 0, 0, &err_msg);
+   if (rc != SQLITE_OK) {
+      fprintf(stderr, "SQL error: %s\n", err_msg);
+      sqlite3_free(err_msg);
+      sqlite3_close(db);
+      return 1;
+   }
+
+   // Insert data
+   const char* insert_sql = 
+      "INSERT INTO Users (Name, Age) VALUES ('Alice', 25);"
+      "INSERT INTO Users (Name, Age) VALUES ('Bob', 30);";
+   
+   rc = sqlite3_exec(db, insert_sql, 0, 0, &err_msg);
+   if (rc != SQLITE_OK) {
+      fprintf(stderr, "SQL error: %s\n", err_msg);
+      sqlite3_free(err_msg);
+      sqlite3_close(db);
+      return 1;
+   }
+
+   // Query and display data
+   printf("User Data:\n");
+   printf("ID\tName\tAge\n");
+   
+   const char* select_sql = "SELECT * FROM Users;";
+   rc = sqlite3_exec(db, select_sql, callback, 0, &err_msg);
+   
+   if (rc != SQLITE_OK) {
+      fprintf(stderr, "SQL error: %s\n", err_msg);
+      sqlite3_free(err_msg);
+   } else {
+
+      printf("Data retrieved successfully.\n");
+   }
 
    sqlite3_close(db);
+    printf("hello world!\r\n");
 
    return 0;
 }
-// int main(int argc, char **argv)
-// {
-//     printf("hello world!\r\n");
-//     return 0;
-// }
